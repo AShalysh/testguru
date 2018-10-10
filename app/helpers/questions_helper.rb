843026d799b83ba5dfc645e21477ce3f13c96913
Anastasia Shalyshkina <anastasia.shalysh@gmail.com>
2018-10-09 17:00:51 -0700

    spaces

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,9 +1,9 @@
 module QuestionsHelper
   def question_header
-  	if @test.present?
-  	  @test.title
-  	else
-  	  @question.test.title
-  	end	
+    if @test.present?
+      @test.title
+    else
+      @question.test.title
+    end
   end
 end
