20a2ed929783f1a52f44002496279bc1e5f1ae23
Anastasia Shalyshkina <anastasia.shalysh@gmail.com>
2018-12-06 01:13:56 -0800

    final changes to the flash

--- app/controllers/my_devise/sessions_controller.rb
+++ app/controllers/my_devise/sessions_controller.rb
@@ -1,8 +1,9 @@
 class MyDevise::SessionsController < Devise::SessionsController
 
   def create
-    set_flash_message :notice, :signed_in, name: current_user.first_name if is_navigational_format?
-    # t('sessions.signed_in', name: current_user.first_name)
-    super
+  	super
+    set_flash_message :notice, :my_signed_in, name: current_user.first_name
   end
 end
+
+#set_flash_message :notice, :signed_in, name: current_user.first_name if is_navigational_format?
